b3fe29dd44edb695b96dc07f269c9089dcfcce66
EdiW31 <129823940+EdiW31@users.noreply.github.com>
2024-11-12 08:37:31 +0200

    Created the entities

---
+++ src/main/java/com/parking/parkinglot/entities/User.java
@@ -0,0 +1,60 @@
+package com.parking.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "user")
+public class User {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    @Basic
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    private Collection<Car> cars;
+
+    @OneToMany(mappedBy = "owner")
+    public Collection<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(Collection<Car> cars) {
+        this.cars = cars;
+    }
+}
